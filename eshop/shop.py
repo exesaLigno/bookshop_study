6391d9d77e3f18692ed54f1f8013d6f936dbc537
Vadim Karcev <cfg888powerfull@gmail.com>
2024-05-05 01:46:22 +0300

    Added cart and some cart methods

--- eshop/shop.py
+++ eshop/shop.py
@@ -11,6 +11,29 @@ class Book:
     publisher: str
     genre: str
 
+class Cart:
+
+    carts_counter = 0
+    
+    def __init__(self, buyer_login: str):
+        self.buyer_login: str = buyer_login
+        self.shop_list: list[Book] = []
+        self.pk: int = self.carts_counter
+        self.carts_counter += 1
+        self.delivery: bool = False
+        self.delivery_adress: str | None = None
+        self.delivery_time: str | None = None
+        self.payment_method: str | None = None
+    
+    def add_book_to_cart(self, book: Book) -> None:
+        self.shop_list.append(book)
+
+    def deliver(self, delivery_adress: str, delivery_time: str, payment_method: str):
+        self.delivery = True
+        self.delivery_adress = delivery_adress
+        self.delivery_time = delivery_time
+        self.payment_method = payment_method
+
 class Shop:
 
     handlers: dict[str, str] = {
@@ -18,6 +41,11 @@ class Shop:
         'выход': 'unlogin',
         'каталог': 'catalog',
         'добавить_товар': 'add_book',
+        'корзина': 'show_cart',
+        'добавить_в_корзину': 'add_book_to_cart',
+        'доставить': 'deliver',
+        'очистить_корзину': 'clear_cart',
+        'заказы': 'orders'
     }
 
     @classmethod
@@ -36,6 +64,7 @@ class Shop:
         self.users: dict[str, bool] = {}
         self.current_user: str | None = None
         self.books_catalog: list[Book] = []
+        self.carts: list[Cart] = []
 
     def process(self, command_line: str) -> str:
         splitted = self.__split_command_line(command_line)
@@ -84,3 +113,60 @@ class Shop:
         new_book = Book(args[0], args[1], int(args[2]), float(args[3]), args[4], args[5])
         self.books_catalog.append(new_book)
         return f'Книга "{new_book.title}" добавлена в каталог!'
+
+    def show_cart(self, args: list[str]) -> str:
+        message = 'Ваша корзина пока пуста.'
+        user_carts = list(filter(lambda cart: cart.buyer_login == self.current_user and not cart.delivery, self.carts))
+        if len(user_carts) != 0 and len(user_carts[0].shop_list) != 0:
+            message = 'Ваша корзина:'
+            for book in user_carts[0].shop_list:
+                message += f'\n\t{book.title} ({book.author}) --- {book.price}'
+        return message
+
+    def add_book_to_cart(self, args: list[str]) -> str:
+        user_carts = list(filter(lambda cart: cart.buyer_login == self.current_user and not cart.delivery, self.carts))
+        if len(user_carts) == 0:
+            new_cart = Cart(self.current_user)
+            self.carts.append(new_cart)
+            cart = new_cart
+        else:
+            cart = user_carts[0]
+        
+        suggested_books = list(filter(lambda book: book.title.lower() == args[0].lower(), self.books_catalog))
+        if len(suggested_books) != 0:
+            cart.add_book_to_cart(suggested_books[0])
+            message = f'Книга "{suggested_books[0].title}" добавлена в вашу корзину.'
+        
+        return message
+
+    def deliver(self, args: list[str]) -> str:
+        user_carts = list(filter(lambda cart: cart.buyer_login == self.current_user and not cart.delivery, self.carts))
+        if len(user_carts) == 0:
+            new_cart = Cart(self.current_user)
+            self.carts.append(new_cart)
+            cart = new_cart
+        else:
+            cart = user_carts[0]
+
+        cart.deliver(args[0], args[1], args[2])
+
+        return f'Доставка оформлена, заказ #{cart.pk}'
+
+    def clear_cart(self, args: list[str]) -> str:
+        user_carts = list(filter(lambda cart: cart.buyer_login == self.current_user and not cart.delivery, self.carts))
+        if len(user_carts) == 0:
+            new_cart = Cart(self.current_user)
+            self.carts.append(new_cart)
+            cart = new_cart
+        else:
+            cart = user_carts[0]
+
+        cart.shop_list = []
+        return 'Ваша корзина очищена.'
+
+    def orders(self, args: list[str]) -> str:
+        orders = list(filter(lambda cart: cart.buyer_login == self.current_user and cart.delivery, self.carts))
+        if len(orders) == 0:
+            return 'У вас нет активных заказов.'
+        else:
+            pass
