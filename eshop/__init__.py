cbe8b451b066602f5e4a830d9e6399c827d25520
Vadim Karcev <cfg888powerfull@gmail.com>
2024-05-07 22:19:21 +0300

    Added delivery feature

--- eshop/__init__.py
+++ eshop/__init__.py
@@ -21,14 +21,21 @@ class Cart:
         self.shop_list: list[Book] = []
         self.primary_key: int = cart_id
         self.delivery: bool = False
+        self.delivery_adress: str | None = None
+        self.delivery_time: str | None = None
+        self.payment_method: str | None = None
 
     def add_book_to_cart(self, book: Book) -> None:
         '''Method for adding new book to cart'''
         self.shop_list.append(book)
 
-    def deliver(self) -> None:
-        '''Deliver contents of a cart'''
-        return None
+    def deliver(self, delivery_adress: str,
+                delivery_time: str, payment_method: str) -> None:
+        '''Deliver cart contents'''
+        self.delivery = True
+        self.delivery_adress = delivery_adress
+        self.delivery_time = delivery_time
+        self.payment_method = payment_method
 
 
 class Shop:
@@ -42,6 +49,8 @@ class Shop:
         'корзина': 'show_cart',
         'добавить_в_корзину': 'add_book_to_cart',
         'очистить_корзину': 'clear_cart',
+        'доставить': 'deliver',
+        'заказы': 'orders',
     }
 
     @classmethod
@@ -175,4 +184,34 @@ class Shop:
             if len(user_carts) != 0:
                 user_carts[0].shop_list = []
 
+        return message
+
+    def deliver(self, args: list[str]) -> str:
+        '''Deliver cart'''
+        user_carts = list(
+            filter(
+                lambda cart: cart.buyer_login == self.current_user and
+                not cart.delivery, self.carts))
+        if len(user_carts) == 0:
+            return 'Ваша корзина пуста, невозможно оформить доставку!'
+        cart = user_carts[0]
+
+        cart.deliver(args[0], args[1], args[2])
+
+        return f'Доставка оформлена, заказ #{cart.primary_key}'
+
+    def orders(self, args: list[str]) -> str:
+        '''Show list of orders'''
+        message = 'В данную функцию не нужно передавать аргументы!'
+        if len(args) == 0:
+            message = 'У вас нет активных заказов.'
+            orders = list(
+                filter(
+                    lambda cart: cart.buyer_login == self.current_user and
+                    cart.delivery, self.carts))
+            if len(orders) != 0:
+                message = 'Ваши заказы:'
+                for order in orders:
+                    message += f'\n\tЗаказ #{order.primary_key}: '
+                    message += f'{order.delivery_time}, {order.payment_method}'
         return message
