32d061b0b5dfeec8abbb7eade8e45057377d687a
Vadim Karcev <cfg888powerfull@gmail.com>
2024-05-10 18:17:23 +0300

    Improved fuzzing with cmd patterns and coverage

---
+++ fuzz.sh
@@ -0,0 +1,2 @@
+poetry run python -m coverage run tests/fuzz_shop.py -atheris_runs=100000
+poetry run python -m coverage report
--- tests/fuzz_shop.py
+++ tests/fuzz_shop.py
@@ -1,18 +1,42 @@
 import atheris
 import sys
-from eshop import Shop
+from random import choice
+with atheris.instrument_imports():
+    from eshop import Shop
+
+commands = [
+    'вход', 'выход', 'каталог', 'добавить_товар', 'корзина', 
+    'добавить_в_корзину', 'очистить_корзину', 'доставить', 'заказы', 
+    'вернуть_заказ', 'все_заказы', 'подтвердить_доставку', 'все_возвраты', 
+    'одобрить_возврат', 'сделать_бочку', 'отклонить_доставку', 'фыва',
+    'биба', 'боба'
+    ]
+
+errors = []
+
+def generate_input(input_bytes):
+    cmd = choice(commands)
+    try:
+        fdp = atheris.FuzzedDataProvider(input_bytes)
+        data = list(fdp.ConsumeUnicode(sys.maxsize).split())
+    except UnicodeEncodeError:
+        data = []
+    inp = [cmd] + data
+    empty_count = inp.count('')
+    for _ in range(empty_count):
+        inp.remove('')
+    return ' '.join(inp)
+    
 
 def test_fuzz(input_bytes):
     try:
-        fdp = atheris.FuzzedDataProvider(input_bytes)
-        data = fdp.ConsumeUnicode(sys.maxsize)
+        data = generate_input(input_bytes)
         shop = Shop()
         result = shop.process(data)
-        # print(f'Input: {data}')
-        # print(f'Result: {result}')
-    except (UnicodeEncodeError, IndexError):
-        pass
+    except Exception as error:
+        errors.append(error)
 
 if __name__ == "__main__":
     atheris.Setup(sys.argv, test_fuzz)
     atheris.Fuzz()
+    print(len(errors))
